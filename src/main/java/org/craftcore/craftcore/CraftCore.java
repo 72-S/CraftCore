6a7338eccc8e2409c1aaeb8df4b1b58cf939c04c
consti <consti.bauer@outlook.com>
2024-03-06 15:05:18 +0100

    base files

---
+++ src/main/java/org/craftcore/craftcore/CraftCore.java
@@ -0,0 +1,10 @@
+package org.craftcore.craftcore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftCore implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/craftcore/craftcore/client/CraftCoreClient.java
@@ -0,0 +1,9 @@
+package org.craftcore.craftcore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
